8f99db25d14e7faeb8c2de777dc37b406ebc8657
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-09-14 16:09:56 +0200

    fix WS API v3 support

--- app/controllers/SocketController.scala
+++ app/controllers/SocketController.scala
@@ -13,11 +13,11 @@ class SocketController @Inject() (
     val controllerComponents: ControllerComponents
 )(implicit ec: ExecutionContext) extends BaseController {
 
-  def site(sri: String): WebSocket = WebSocket { req =>
+  def site(sri: String, apiVersion: Int): WebSocket = WebSocket { req =>
     server.connectToSite(req, Sri(sri), flagOf(req)) map Right.apply
   }
 
-  def lobby(sri: String): WebSocket = WebSocket { req =>
+  def lobby(sri: String, apiVersion: Int): WebSocket = WebSocket { req =>
     server.connectToLobby(req, Sri(sri), flagOf(req)) map Right.apply
   }
 }
