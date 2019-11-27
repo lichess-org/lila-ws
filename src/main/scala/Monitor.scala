2f7edb9aca9ccd2a6fa716428fdf6d3a780792cc
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-11-26 20:45:53 -0600

    fix monitoring name

--- src/main/scala/Monitor.scala
+++ src/main/scala/Monitor.scala
@@ -77,7 +77,7 @@ object Monitor {
   val usersSize = Kamon.gauge("users.size").withoutTags
   val watchSize = Kamon.gauge("watch.size").withoutTags
 
-  val palantirChannels = Kamon.gauge("crowd.channels.size").withoutTags
+  val palantirChannels = Kamon.gauge("palantir.channels.size").withoutTags
 
   val busSize = Kamon.gauge("bus.size").withoutTags
   val busAllSize = Kamon.gauge("bus.all.size").withoutTags
