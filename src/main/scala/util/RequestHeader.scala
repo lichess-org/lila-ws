4aa061dc4ad059bcd8f50e1de6c9693af9460ea4
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-01-02 22:01:00 +0100

    HttpHeaders.Names (deprecated) -> HttpHeaderNames

--- src/main/scala/util/RequestHeader.scala
+++ src/main/scala/util/RequestHeader.scala
@@ -2,7 +2,7 @@ package lila.ws
 package util
 
 import io.netty.handler.codec.http.cookie.ServerCookieDecoder
-import io.netty.handler.codec.http.{ HttpHeaders, QueryStringDecoder }
+import io.netty.handler.codec.http.{ HttpHeaders, HttpHeaderNames, QueryStringDecoder }
 import scala.jdk.CollectionConverters._
 
 final class RequestHeader(uri: String, req: HttpHeaders, val ip: IpAddress) {
@@ -11,12 +11,12 @@ final class RequestHeader(uri: String, req: HttpHeaders, val ip: IpAddress) {
 
   def path = query.path
 
-  def header(name: String): Option[String] =
+  def header(name: CharSequence): Option[String] =
     Option(req get name).filter(_.nonEmpty)
 
   def cookie(name: String): Option[String] =
     for {
-      encoded <- header(HttpHeaders.Names.COOKIE)
+      encoded <- header(HttpHeaderNames.COOKIE)
       cookies = ServerCookieDecoder.LAX decode encoded
       cookie <- cookies.asScala.find(_.name contains name)
       value  <- Some(cookie.value).filter(_.nonEmpty)
@@ -28,9 +28,9 @@ final class RequestHeader(uri: String, req: HttpHeaders, val ip: IpAddress) {
   def queryParameterInt(name: String): Option[Int] =
     queryParameter(name) flatMap (_.toIntOption)
 
-  def userAgent: String = header(HttpHeaders.Names.USER_AGENT) getOrElse ""
+  def userAgent: String = header(HttpHeaderNames.USER_AGENT) getOrElse ""
 
-  def origin: Option[String] = header(HttpHeaders.Names.ORIGIN)
+  def origin: Option[String] = header(HttpHeaderNames.ORIGIN)
 
   def flag: Option[Flag] = queryParameter("flag") flatMap Flag.make
 
