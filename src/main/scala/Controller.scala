811c65afeb8bb319bf28114e09c35ca2cb202d50
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-01-04 09:49:50 +0100

    Revert "switch to list of csrf origins (for ornicar/lila#5869)"

--- src/main/scala/Controller.scala
+++ src/main/scala/Controller.scala
@@ -5,7 +5,6 @@ import com.typesafe.scalalogging.Logger
 import io.netty.handler.codec.http.HttpResponseStatus
 import scala.concurrent.duration._
 import scala.concurrent.{ ExecutionContext, Future }
-import scala.jdk.CollectionConverters._
 
 import util.RequestHeader
 
@@ -174,7 +173,7 @@ final class Controller(
 
   private object CSRF {
 
-    val csrfOrigins = config.getStringList("csrf.origins").asScala.toSet
+    val csrfOrigin = config.getString("csrf.origin")
     val appOrigins = Set(
       "ionic://localhost",     // ios
       "capacitor://localhost", // capacitor (ios next)
@@ -187,8 +186,8 @@ final class Controller(
 
     def check(req: RequestHeader)(f: => Response): Response =
       req.origin match {
-        case None                                                      => f // for exotic clients and acid ape chess
-        case Some(origin) if csrfOrigins(origin) || appOrigins(origin) => f
+        case None                                                       => f // for exotic clients and acid ape chess
+        case Some(origin) if origin == csrfOrigin || appOrigins(origin) => f
         case Some(origin) =>
           logger.debug(s"""CSRF origin: "$origin" ${req.name}""")
           Future successful Left(HttpResponseStatus.FORBIDDEN)
