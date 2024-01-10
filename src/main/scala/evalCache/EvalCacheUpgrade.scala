37a0ab41598a6b01d68485a76b1baf1dff9be35b
Thibault Duplessis <t@lichess.org>
2024-01-10 12:43:48 +0100

    scala tweak

--- src/main/scala/evalCache/EvalCacheUpgrade.scala
+++ src/main/scala/evalCache/EvalCacheUpgrade.scala
@@ -36,21 +36,21 @@ final private class EvalCacheUpgrade(using
     expirableSris put sri
 
   def onEval(input: EvalCacheEntry.Input, fromSri: Sri): Unit =
-    (1 to input.eval.multiPv.value) flatMap { multiPv =>
-      val setupId = makeSetupId(input.id.variant, input.fen, MultiPv(multiPv))
-      evals get setupId map (setupId -> _)
-    } filter {
-      _._2.depth < input.eval.depth
-    } foreach { (setupId, eval) =>
-      evals += (setupId -> eval.copy(depth = input.eval.depth))
-      val wms = eval.sris.withFilter(_ != fromSri) flatMap { sri => members.get(sri.value) }
-      if wms.nonEmpty then
-        val evalJson = EvalCacheJsonHandlers.writeEval(input.eval, input.fen)
-        wms.groupBy(_.path) map: (path, wms) =>
-          val hit = EvalHit(evalJson + ("path" -> JsString(path.value)))
-          wms foreach { wm => Bus.publish(_ sri wm.sri, hit) }
-        upgradeMon.count.increment(wms.size)
-    }
+    (1 to input.eval.multiPv.value)
+      .flatMap: multiPv =>
+        val setupId = makeSetupId(input.id.variant, input.fen, MultiPv(multiPv))
+        evals get setupId map (setupId -> _)
+      .filter:
+        _._2.depth < input.eval.depth
+      .foreach: (setupId, eval) =>
+        evals += (setupId -> eval.copy(depth = input.eval.depth))
+        val wms = eval.sris.withFilter(_ != fromSri) flatMap { sri => members.get(sri.value) }
+        if wms.nonEmpty then
+          val evalJson = EvalCacheJsonHandlers.writeEval(input.eval, input.fen)
+          wms.groupBy(_.path) map: (path, wms) =>
+            val hit = EvalHit(evalJson + ("path" -> JsString(path.value)))
+            wms foreach { wm => Bus.publish(_ sri wm.sri, hit) }
+          upgradeMon.count.increment(wms.size)
 
   private def expire(sri: Sri): Unit =
     members get sri.value foreach { wm =>
