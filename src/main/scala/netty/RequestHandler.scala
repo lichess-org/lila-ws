c2446fa5f557aabedbcc15c7c9d9543ab2eacb5d
Thanh Le <lenguyenthanh@hotmail.com>
2024-02-23 09:44:57 +0700

    Catch exceptions when parsing request header
    Also make all class variables lazy just in case we don't use them

--- src/main/scala/netty/RequestHandler.scala
+++ src/main/scala/netty/RequestHandler.scala
@@ -18,28 +18,30 @@ final private class RequestHandler(router: Router)(using Executor)
     ):
 
   override def channelRead0(ctx: ChannelHandlerContext, req: FullHttpRequest): Unit =
-    val request = util.RequestHeader(RequestUri(req.uri), req.headers)
-    router(request) foreach:
-      case Left(status) =>
-        sendErrorResponse(
-          ctx,
-          DefaultFullHttpResponse(req.protocolVersion(), status, Unpooled.EMPTY_BUFFER)
-        )
-        req.release()
-      case Right(_) if req.method != HttpMethod.GET =>
-        val response = DefaultFullHttpResponse(
-          req.protocolVersion(),
-          HttpResponseStatus.METHOD_NOT_ALLOWED,
-          Unpooled.EMPTY_BUFFER
-        )
-        response.headers.set(HttpHeaderNames.ALLOW, "GET")
-        sendErrorResponse(ctx, response)
-        req.release()
-      case Right(endpoint) =>
-        Monitor.mobile.connect(request)
-        // Forward to ProtocolHandler with endpoint attribute
-        ctx.channel.attr(ProtocolHandler.key.endpoint).set(endpoint)
-        ctx.fireChannelRead(req)
+    util
+      .RequestHeader(RequestUri(req.uri), req.headers)
+      .foreach: request =>
+        router(request).foreach:
+          case Left(status) =>
+            sendErrorResponse(
+              ctx,
+              DefaultFullHttpResponse(req.protocolVersion(), status, Unpooled.EMPTY_BUFFER)
+            )
+            req.release()
+          case Right(_) if req.method != HttpMethod.GET =>
+            val response = DefaultFullHttpResponse(
+              req.protocolVersion(),
+              HttpResponseStatus.METHOD_NOT_ALLOWED,
+              Unpooled.EMPTY_BUFFER
+            )
+            response.headers.set(HttpHeaderNames.ALLOW, "GET")
+            sendErrorResponse(ctx, response)
+            req.release()
+          case Right(endpoint) =>
+            Monitor.mobile.connect(request)
+            // Forward to ProtocolHandler with endpoint attribute
+            ctx.channel.attr(ProtocolHandler.key.endpoint).set(endpoint)
+            ctx.fireChannelRead(req)
 
   private def sendErrorResponse(ctx: ChannelHandlerContext, response: DefaultFullHttpResponse) =
     val f = ctx.writeAndFlush(response)
