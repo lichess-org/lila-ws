34bb3b6110b22dadb0e15705006c7ca2b26fe074
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2024-02-26 19:51:51 -0500

    Fix deprecation warnings

--- src/main/scala/SocialGraph.scala
+++ src/main/scala/SocialGraph.scala
@@ -3,7 +3,7 @@ package lila.ws
 import com.typesafe.config.Config
 import java.util.concurrent.locks.ReentrantLock
 import scala.jdk.CollectionConverters.*
-import scala.util.control.NonLocalReturns.*
+import scala.util.boundary
 import scala.util.Random
 
 // Best effort fixed capacity cache for the social graph of online users.
@@ -54,16 +54,16 @@ final class SocialGraph(mongo: Mongo, config: Config):
       (Integer.rotateLeft(state, 5) ^ ch.toInt) * 0x9e3779b9
     }
 
-  private def findSlot(id: User.Id, exceptSlot: Int): Slot = returning[Slot]:
+  private def findSlot(id: User.Id, exceptSlot: Int): Slot = boundary[Slot]:
     // Try to find an existing or empty slot between hash and
     // hash + MaxStride.
     val hash = fxhash32(id) & slotsMask
     for s <- hash to (hash + SocialGraph.MaxStride) do
       val slot = s & slotsMask
       read(slot) match
-        case None => throwReturn[Slot](NewSlot(slot))
+        case None => boundary.break[Slot](NewSlot(slot))
         case Some(existing) if existing.id == id =>
-          throwReturn[Slot](ExistingSlot(slot, existing))
+          boundary.break[Slot](ExistingSlot(slot, existing))
         case _ =>
 
     // If no existing or empty slot is available, try to replace an
@@ -75,11 +75,11 @@ final class SocialGraph(mongo: Mongo, config: Config):
     for s <- hash to (hash + SocialGraph.MaxStride) do
       val slot = s & slotsMask
       read(slot) match
-        case None => throwReturn[Slot](NewSlot(slot))
+        case None => boundary.break[Slot](NewSlot(slot))
         case Some(existing) if existing.id == id =>
-          throwReturn[Slot](ExistingSlot(slot, existing))
+          boundary.break[Slot](ExistingSlot(slot, existing))
         case Some(existing) if !existing.meta.online && slot != exceptSlot =>
-          throwReturn[Slot](freeSlot(slot))
+          boundary.break[Slot](freeSlot(slot))
         case _ =>
 
     // The hashtable is full. Overwrite a random entry.
--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -18,7 +18,7 @@ type Client         = ActorRef[ipc.ClientMsg]
 type ClientEmit     = Emit[ipc.ClientIn]
 
 type ~[+A, +B] = Tuple2[A, B]
-object ~ :
+object `~`:
   def apply[A, B](x: A, y: B)                              = Tuple2(x, y)
   def unapply[A, B](x: Tuple2[A, B]): Option[Tuple2[A, B]] = Some(x)
 
