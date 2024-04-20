7a915640ddff7946b7af92a0d79a73338511e041
Ty Chermsirivatana <chermsit@dickinson.edu>
2024-04-20 01:41:54 -0400

    Update Lag.scala

--- src/main/scala/Lag.scala
+++ src/main/scala/Lag.scala
@@ -1,7 +1,6 @@
 package lila.ws
 
 import com.github.blemale.scaffeine.{ Cache, Scaffeine }
-
 import lila.ws.ipc.LilaIn
 import lila.ws.util.Domain
 
@@ -13,24 +12,26 @@ final class Lag(lilaRedis: Lila, groupedWithin: util.GroupedWithin):
   private val maxMillis = 5000
 
   private val trustedStats: Cache[User.Id, TrustedMillis] = Scaffeine()
-    .expireAfterWrite(1 hour)
+    .expireAfterWrite(1.hour)
     .build[User.Id, TrustedMillis]()
 
   export trustedStats.getIfPresent as sessionLag
 
-  private val clientReports = groupedWithin[(User.Id, Int)](256, 947.millis): lags =>
+  private val clientReports = groupedWithin[(User.Id, Int)](256, 947.millis) { lags =>
     lilaRedis.emit.site(LilaIn.Lags(lags.toMap))
+  }
 
   export clientReports.apply as recordClientLag
 
   def recordTrustedLag(millis: Int, userId: Option[User.Id], domain: Domain) =
     Monitor.lag.roundFrameLag(millis, domain)
-    userId.foreach: uid =>
+    userId.foreach { uid =>
       trustedStats.put(
         uid,
-        sessionLag(uid)
-          .fold(millis): prev =>
-            val weight = if (millis < prev) trustedNormalRefreshFactor else trustedSpikeRefreshFactor
-            val cappedMillis = millis.min(maxMillis)
-            (prev * (1 - weight) + cappedMillis * weight).toInt
+        sessionLag(uid).fold(millis) { prev =>
+          val weight = if (millis < prev) trustedNormalRefreshFactor else trustedSpikeRefreshFactor
+          val cappedMillis = millis.min(maxMillis)
+          (prev * (1 - weight) + cappedMillis * weight).toInt
+        }
       )
+    }
