061a49e78b101f533e2f6b232c93cf4e3c583083
Thibault Duplessis <t@lichess.org>
2024-12-28 18:24:45 +0100

    compat for lichess mobile: don't send study messages in batches

--- src/main/scala/actor/RoomActor.scala
+++ src/main/scala/actor/RoomActor.scala
@@ -47,10 +47,15 @@ object RoomActor:
       None -> None
 
     case ClientIn.VersionedBatch(msgs) =>
-      deps.clientIn:
-        ClientIn.PayloadBatch:
-          msgs.toList.reverse.map: msg =>
-            versionFor(state.isTroll, msg).json
+      if deps.req.isLichessMobile // BC, remove this in a month
+      then
+        msgs.toList.reverse.foreach: msg =>
+          deps.clientIn(versionFor(state.isTroll, msg))
+      else
+        deps.clientIn:
+          ClientIn.PayloadBatch:
+            msgs.toList.reverse.map: msg =>
+              versionFor(state.isTroll, msg).json
       None -> None
 
     case ClientIn.OnlyFor(endpoint, payload) =>
