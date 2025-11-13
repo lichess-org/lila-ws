17d84255d517df3dd895e0f2acfb02c37b967766
Thibault Duplessis <t@lichess.org>
2025-11-13 08:55:35 +0100

    import withTimeout from scalalib, and after from pekko

---
+++ src/main/scala/util/LilaFuture.scala
@@ -0,0 +1,38 @@
+package lila.ws
+package util
+
+import scala.util.control.NonFatal
+import scalalib.future.TimeoutException
+
+object LilaFuture:
+
+  // implementation borrowed from pekko,
+  // but using the typed scheduler instead of the classic one
+  def after[T](duration: FiniteDuration)(
+      value: => Future[T]
+  )(using ec: Executor, scheduler: Scheduler): Future[T] =
+    if duration.isFinite && duration.length < 1 then
+      try value
+      catch case NonFatal(t) => Future.failed(t)
+    else
+      val p = Promise[T]()
+      scheduler.scheduleOnce(
+        duration,
+        () =>
+          p.completeWith {
+            try value
+            catch case NonFatal(t) => Future.failed(t)
+          }
+      )
+      p.future
+
+extension [A](fua: Future[A])
+
+  // like scalalib.future.withTimeout, but using pekko's typed scheduler
+  def withTimeout(duration: FiniteDuration, error: => String)(using Executor, Scheduler): Future[A] =
+    Future.firstCompletedOf(
+      Seq(
+        fua,
+        LilaFuture.after(duration)(Future.failed(TimeoutException(s"$error timeout after $duration")))
+      )
+    )
